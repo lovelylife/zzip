86e02e386b25c50924e8ca8f73816d5c5e7beb13
life.qm <life.QM@gmail.com>
2011-11-17 11:07:21 +0000

    解决ZZipFile二进制读写文件的问题 增加ZZipFileObject相关接口，取文件信息

--- apps/ZZip/ZZipFileObject.h
+++ apps/ZZip/ZZipFileObject.h
@@ -13,7 +13,13 @@ public:
 	~ZZipFileObject(void);
 
 	bool ZZipPathFromPath( const tstring& sPath);
-	bool IsFolder();
+	bool isfolder();
+	uint64 filesize() const;
+	tstring path() const;
+	tstring localpath() const;
+	int32 createtime() const;
+	int32 lastmodify() const;
+
 	friend class ZZipFile;
 private: 
 	ZZipFileItem FileItem_;
