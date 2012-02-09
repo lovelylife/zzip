696188e72ea4a33039033f0b58642b6b240c1303
life.QM@gmail.com <life.QM@gmail.com@59005abb-f2e6-0014-b23c-3a465410172c>
2012-02-09 15:20:43 +0000

    这两个文件临时用，非工程文件

---
+++ ZZipSamples/Color.cpp
@@ -0,0 +1,30 @@
+
+#include "Stdafx.h"
+#include "Color.h"
+
+static Color* Color::FromStringRRGGBB(const tstring& sRRGGBB) {
+
+	Color* NewColorObject = new Color;
+	// ½âÎö×Ö·û´®
+}
+
+static Color* Color::FromStringRGBVVV(const tstring& sRGBVVV) {
+
+}
+
+Color::Color() {}
+
+// Color::Color(unsigned char r, unsigned char g, unsigned char b, unsigned char a);
+// Color::Color(const Color&);
+// Color Color::operator =(const Color&);
+// operator Color::COLORREF() const;
+// 
+// tstring Color::Format2RRGGBB();
+// tstring Color::Format2RGBVVV();
+
+
+	
+}; // end namespace QUI
+
+
+#endif
