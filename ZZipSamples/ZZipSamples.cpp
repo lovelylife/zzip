a793449bc5e2d86baf90d24c8215a83d1b368f03
life.qm <life.QM@gmail.com>
2012-05-22 16:03:33 +0000

    解决ZZip文件夹遍历问题

--- ZZipSamples/ZZipSamples.cpp
+++ ZZipSamples/ZZipSamples.cpp
@@ -51,18 +51,20 @@ int _tmain(int argc, _TCHAR* argv[]) {
 	std::cout << sString.size() << std::endl;
 	std::cout << sizeof(ZZipFileItem) << std::endl;
 	ZZipFile zzip;
-	if(argc <= 1) { return 0; }
-	if(zzip.Open(argv[1])) {
-		zzip.AddFolder(_T("/"), _T("D:\\test"));
-		zzip.AddFile(_T("/1.txt"), _T("C:\\1.txt"));
-		zzip.AddFile(_T("/2.txt"), _T("C:\\2.txt"));
-		zzip.AddFile(_T("/3.txt"), _T("C:\\3.txt"));
-		zzip.AddFile(_T("/4.gif"), _T("C:\\4.gif"));
+	// if(argc <= 1) { return 0; }
+	tstring sZZipFileName = _T("D:/Developing/VCCode/trunk/QUI/QUITest/res/skin.zzip");
+	// if(zzip.Open(argv[1])) {
+	if(zzip.Open(sZZipFileName)) {
+		zzip.AddFolder(_T("/"), _T("D:/Developing/VCCode/trunk/QUI/QUITest/res/skin"));
+// 		zzip.AddFile(_T("/1.txt"), _T("C:\\1.txt"));
+// 		zzip.AddFile(_T("/2.txt"), _T("C:\\2.txt"));
+// 		zzip.AddFile(_T("/3.txt"), _T("C:\\3.txt"));
+// 		zzip.AddFile(_T("/4.gif"), _T("C:\\4.gif"));
 		zzip.Save();
 		zzip.Close();
 	}
 
-	if(zzip.Open(argv[1])) {
+	if(zzip.Open(sZZipFileName)) {
 
 		// 枚举文件
 // 		EnumItems e;
@@ -72,29 +74,29 @@ int _tmain(int argc, _TCHAR* argv[]) {
 // 		evt -= std::make_pair(&e, &EnumItems::EnumFile);
 
 		ZZipEnum<EnumItems>(zzip, _T("/"), true);
-
-		// 读取文件"/4.gif" 保存到 "C:\\tt.gif"
-		RefPtr<ZZipFileObject> p = zzip.FindFile(_T("/4.gif"));
-		if(NULL != p) {
-			std::ofstream of(_T("C:\\tt.gif"), std::ios::out|std::ios::binary);
-			if(of.good()) {
-				char buffer[1024] = {0};
-				int64 filesize = p->filesize();
-				int64 startpos = 0;
-				int64 fcount = filesize;
-				while(fcount > 0) {
-					int64 ReadBytes = zzip.ReadData(p, startpos, (void*)buffer, 1024);
-					if(ReadBytes > 0) {
-						fcount -= ReadBytes;
-						startpos += ReadBytes;
-						of.write(buffer, ReadBytes);
-					} else {
-						break;
-					}
-				}
-				of.close();
-			}
-		}
+// 
+// 		// 读取文件"/4.gif" 保存到 "C:\\tt.gif"
+// 		RefPtr<ZZipFileObject> p = zzip.FindFile(_T("/4.gif"));
+// 		if(NULL != p) {
+// 			std::ofstream of(_T("C:\\tt.gif"), std::ios::out|std::ios::binary);
+// 			if(of.good()) {
+// 				char buffer[1024] = {0};
+// 				int64 filesize = p->filesize();
+// 				int64 startpos = 0;
+// 				int64 fcount = filesize;
+// 				while(fcount > 0) {
+// 					int64 ReadBytes = zzip.ReadData(p, startpos, (void*)buffer, 1024);
+// 					if(ReadBytes > 0) {
+// 						fcount -= ReadBytes;
+// 						startpos += ReadBytes;
+// 						of.write(buffer, ReadBytes);
+// 					} else {
+// 						break;
+// 					}
+// 				}
+// 				of.close();
+// 			}
+// 		}
 		
 		zzip.Close();
 	}
