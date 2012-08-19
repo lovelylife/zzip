79e1d5f45904c52bc8829089559677913870645f
life.QM@gmail.com <life.QM@gmail.com@59005abb-f2e6-0014-b23c-3a465410172c>
2012-08-19 15:25:19 +0000

    构建线程池

--- qdownloader/include/Downloaders.h
+++ qdownloader/include/Downloaders.h
@@ -7,10 +7,48 @@
 
 namespace q {
 
+struct ITask : Object {
+	virtual bool work() = 0;
+	virtual bool onfinish() = 0;
+};
+
+class ThreadPool {
+
+};
+
+class ThreadPoolManager {
+public:
+	ThreadPoolManager() {
+		number_of_thread = 5;
+	}
+
+	ThreadPoolManager(int thread_num) 
+	: number_of_thread(thread_num)
+	{
+		if(number_of_thread <= 0) {
+			number_of_thread = 5;
+		}
+	}
+
+	bool run() {
+		thread_pool_ = new ThreadPool(number_of_thread);
+	}
+private:
+	ThreadPool* thread_pool_;
+	int number_of_thread;
+};
+
+
+class Reactor {
+
+	std::list<list> list;
+};
+
 class IDownloaders : public Object {
 public:
 	virtual void initialize() = 0;
 	virtual long create_task(const char* sUrl, const char* sSavePath, IDownloadController* control) = 0;
+	virtual void remove_task(const char* sUrl);
 };
 
 static IDownloaders* create_downloaders();
