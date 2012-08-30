6bce23fca4bcef6b1ea86f7c966750f85f72eccc
life.QM@gmail.com <life.QM@gmail.com@59005abb-f2e6-0014-b23c-3a465410172c>
2012-08-30 14:27:22 +0000

    实现QHttp下载和Request功能

--- QHttp/include/QHttp.h
+++ QHttp/include/QHttp.h
@@ -8,11 +8,11 @@ namespace q {
 
 class Http : public Object {
 public:
-	virtual long request (const char* sUrl, IRequestController*);
+	virtual long request (const char* sUrl, IRequestController*) = 0;
 	virtual long download(const char* sUrl, const char* sSavePath, IDownloadController* control) = 0;
 };
 
-Http* create_qhttp(int thread_number);
+Http* create(int thread_number);
 
 } // namespace q
 
--- QHttp/include/QHttpObject.h
+++ QHttp/include/QHttpObject.h
@@ -15,26 +15,25 @@ namespace q {
 // 	virtual ~IHttpObject() {};
 //};
 
-class IHttpResponse : public Object {
+class IHttpResponse : virtual public Object {
 public:
 	virtual const char* url() = 0;
 	virtual int status() = 0;
 	virtual const char* header(const char*) = 0;
-	virtual uint64 size() = 0;
 	virtual ~IHttpResponse() {}
 };
 
 class IDownloadObject : public IHttpResponse {
 public:
-	virtual uint64 downloaded_size() = 0;
 	virtual const char* actual_url() = 0;
-	virtual bool completed() = 0;
+	virtual uint64 file_size() = 0;
+	virtual uint64 downloaded_size() = 0;	
 	virtual ~IDownloadObject() {}
 };
 
 class IRequestObject : public IHttpResponse {
 public:
-	virtual const char* get_content() = 0;
+	virtual const char* content() = 0;
 	virtual ~IRequestObject() {}
 };
 
