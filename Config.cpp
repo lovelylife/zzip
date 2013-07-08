6c6f66e76167c9e05c1211ccd791a7cc6829050c
life.qm <life.QM@gmail.com>
2013-07-08 05:46:20 +0000

    整理Config类

---
+++ Config.cpp
@@ -0,0 +1,101 @@
+//////////////////////////////////////////////////////////////////////////
+// Config.cpp
+// Author: Q
+// Date: 2013-07-07
+// 使用的jsoncpp从这里下载http://sourceforge.net/projects/jsoncpp/ 
+// 博客地址：http://www.cnblogs.com/lovelylife
+
+#include "Config.h"
+#include <fstream>
+
+
+Config::Config(void) :
+value_(Json::objectValue)
+{
+
+}
+
+
+Config::~Config(void)
+{
+
+}
+
+bool Config::FromFile( const std::string& sFile )
+{
+	if(sFile.empty()) 
+		return false;
+
+	std::ifstream f(sFile.c_str());
+	f.seekg(0, std::ios::beg);
+	f.seekg(std::ios::end);
+	std::streamsize file_size = f.gcount();
+	if(file_size <= 0)
+		return false;
+
+	char* buffer = new char[file_size];
+	f.read(buffer, file_size);
+	f.close();
+	bool b = FromString(buffer);
+	delete buffer;
+	buffer = NULL;
+
+	return b;
+}
+
+bool Config::FromString( const std::string& sBuffer )
+{
+	Json::Reader reader;
+	if(!reader.parse(sBuffer, value_)) {
+		return false;
+	}
+
+	return true;
+}
+
+std::string Config::ToString()
+{
+	Json::StyledWriter writer;
+	return writer.write(value_);
+}
+
+bool Config::ToFile( const std::string& sSaveFile )
+{
+	if(sSaveFile.empty()) 
+		return false;
+
+	std::string sOut = ToString();
+
+	std::ofstream f(sSaveFile.c_str());
+	if(!f.good()) {
+		return false;
+	}
+
+	f.write(sOut.c_str(), sOut.size());
+	f.close();
+
+	return true;
+}
+
+
+#ifdef USING_TestCode
+class TestCode_Config {
+public:
+	TestCode_Config() {
+		Config config;
+		config.FromString("{}");
+		config.Set("test.base.savepath", "D:/test");
+
+		Json::Value v = config.Get("base.savepath", "");
+		std::string s = v.asString();
+		OutputDebugStringA(s.c_str());
+		std::string sJsonString;
+		sJsonString = config.ToString();
+		OutputDebugStringA(sJsonString.c_str());
+	}
+
+	~TestCode_Config() {
+		
+	}
+} TestCode_Config_c;
+#endif
--- ZZipSamples/ZZipSamples.cpp
+++ ZZipSamples/ZZipSamples.cpp
@@ -7,6 +7,7 @@
 #include <atlconv.h>
 #include "QHttp.h"
 #include <assert.h>
+#include "../Config.h"
 
 
 // 枚举文件
@@ -38,64 +39,6 @@ struct tests {
 	bool a7 : 1;
 	bool a8 : 1;
 };
-
-class DownloadController : public q::IDownloadController {
-public:
-	void OnAttach(q::IDownloadObject* p) {
-		printf("DownloadController attached.\r\n");
-		object_ = p;
-	}
-
-	void OnDettach() {
-		printf("DownloadController deattached.\r\n");
-	}
-
-	void OnStatusChanged(int) {
-		printf("DownloadController OnStatusChanged.\r\n");
-	}
-
-	size_t OnRecvData(void* ptr, size_t size) {
-		return size;
-	}
-
-	bool IsCompleted() {
-		assert(object_);
-		return (object_->downloaded_size() == object_->file_size() && object_->downloaded_size() > 0);
-	}
-
-	uint64 get_download() {
-		assert(object_);
-		return object_->downloaded_size();
-	}
-
-	uint64 get_total_size() {
-		assert(object_);
-		return object_->file_size();
-	}
-
-	void Dump() {
-		std::ostringstream os;
-		os << "url:\t\t" << object_->url() << std::endl;
-		os << "actual url:\t" << object_->actual_url() << std::endl;
-		os << "size:\t\t" << object_->file_size() << " bytes" << std::endl;
-		printf(os.str().c_str());
-	}
-
-private:
-	RefPtr<q::IDownloadObject> object_;
-};
-
-class HttpClientRequest : public q::IRequestController {
-public:
-	void OnFinish(q::IRequestObject* p) {
-		if(p->status() != 200) {
-			std::cout << "error status code: " << p->status() << std::endl;
-			return;
-		}
-		const char* sContent = p->content();
-	}
-};
-
 
 
 class Interface1 {
@@ -171,157 +114,17 @@ public:
 #include <vector>
 
 
-long CalculateResolutionHeight(const std::vector<POINT>& v) {
 
-	int a = 119;
-	std::cout << a/40 << ": " <<a%40 << std::endl;
-
-
-	return 0;
-	bool is_equal = false;
-	long result_height = 480;
-	long height_less(0), height_larger(0xff);
-	for(std::vector<POINT>::const_iterator it=v.begin();
-		it != v.end(); it++) 
-	{
-		POINT pt = *it;
-		if(pt.y == 480) {
-			is_equal = true;
-			break;
-		} else if(pt.y < 480) {
-			if(pt.y > height_less) {
-				height_less = pt.y;
-			}
-		} else {
-			if(pt.y < height_larger) {
-				height_larger = pt.y;
-			}
-		}
-	}
-
-	if(!is_equal) {
-		if(height_less >= 480) {
-			result_height = height_larger;
-		} else {
-			result_height = height_less;
-		}
-	}
-
-	return result_height;
-}
 
 int _tmain(int argc, _TCHAR* argv[]) {
 	
-	/*
-	long x(0), y(0);
-	// 计算合适的分辨率 640*480
-	std::vector<POINT> resolutions;
-	POINT pt = {640, 480};
-	//resolutions.push_back(pt);
+	Config config;
+	config.Set("images.savepath", "D:\\");
+	config.ToFile("config.json");
 
-	pt.x = 300; pt.y = 380;
-	resolutions.push_back(pt);
-
-	pt.x = 1100; pt.y = 200;
-	resolutions.push_back(pt);
-
-<<<<<<< .mine
-	pt.x = 1280; pt.y = 1024;
-	resolutions.push_back(pt);
-
-	std::vector<POINT> vector_width_less;
-	std::vector<POINT> vector_width_equal;
-	std::vector<POINT> vector_width_larger;
-	long width_larger(0xffff), width_less(0);
-	for(std::vector<POINT>::const_iterator it=resolutions.begin();
-		it != resolutions.end(); it++) 
-=======
-	RefPtr<HttpClientRequest> request_controller = new HttpClientRequest();
-	// 
-	if(0 != downloaders->request(
-		"http://localhost/phpkind/?app=cdmusic&mod=service&inajax=true&action=top100", 
-		request_controller
-		)) 
->>>>>>> .r103
-	{
-		POINT pt = *it;
-		if(pt.x == 640) {
-			vector_width_equal.push_back(pt);
-		} else if( pt.x < 640) {
-			if(pt.x>width_less) {
-				width_less = pt.x;
-				vector_width_less.clear();
-				vector_width_less.push_back(pt);
-			} else if( pt.x == width_less) {
-				vector_width_less.push_back(pt);
-			}
-		} else if( pt.x > 640) {
-			if(pt.x < width_larger) {
-				width_larger = pt.x;
-				vector_width_larger.clear();
-				vector_width_larger.push_back(pt);
-			} else if(pt.x == width_larger) {
-				vector_width_larger.push_back(pt);
-			}
-		}
-	}
-
-	if(vector_width_equal.size() > 0) {
-		x = 640;
-		y = CalculateResolutionHeight(vector_width_equal);
-	} else if(vector_width_less.size() >0) {
-		x = width_less;
-		y = CalculateResolutionHeight(vector_width_less);
-	} else if(vector_width_larger.size()) {
-		x = width_larger;
-		y = CalculateResolutionHeight(vector_width_larger);
-	} else {
-		// not get here
-		assert(0);
-	}
-
-	std::cout << "width:" << x << " height:" << y << std::endl;
-	
-	*/
 	//RefPtr<Z> z = new Z;
 //	CC<int> c;
 //	c.p();
-	q::Http* downloaders = q::create(3);
-
- 	RefPtr<DownloadController> controller = new DownloadController;
-	controller->open("http://sq2.newhua.com/down/LeapFTP3.0.1.46_yfy.zips", "get");
- 	if(0 != downloaders->send(controller)) 
- 	{
- 		printf("error create download task.");
- 	}
-
-//	RefPtr<RequestTop100> request_controller = new RequestTop100();
-//	// 
-//	if(0 != downloaders->request(
-//		"http://192.168.10.106/trunk/?app=cdmusic&mod=service&inajax=true&action=top100", 
-//		request_controller
-//		)) 
-//	{
-//		printf("error create download task.");
-//	}
-//
-//
-	std::streambuf* old = std::cout.rdbuf();
-
-	while(true) {
-		Sleep(1000);
- 		system("cls");
- 		std::cout << "downloaded: " << controller->get_download() << " bytes";
- 		
- 		if(controller->IsCompleted()) {
- 			printf("Download Completed.\r\n");
- 			break;
- 		}
-	}
-
-	controller->Dump();
-
-	return 0;
 // #ifdef USING_TestCode
 // 	QUI::Color::doTest();
 // #endif
